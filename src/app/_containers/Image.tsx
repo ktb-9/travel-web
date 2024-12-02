bfe437dbb69f18e9fbccdcb469038340b45969b1
Kwangyoulsagong <61825101+kwangyoulsagong@users.noreply.github.com>
2024-12-02 12:53:05 +0900

    Revert "Feat2/add link"

--- src/app/_containers/Image.tsx
+++
@@ -1,9 +0,0 @@
-import Content from "../_components/content/content";
-
-export default function Image() {
-  return (
-    <div className="absolute flex flex-col w-full h-full">
-      <Content />
-    </div>
-  );
-}
